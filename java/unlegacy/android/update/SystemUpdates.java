4d73ae31fc0500fc492a8d95a740d09909e74ddc
André Pinela <sheffzor@gmail.com>
2016-10-13 19:36:14 +0100

    Hail Unlegacy Android FOTA!!
    Change-Id: I8f1f77f5f0b900cbb1259381a41e3f4238c0831f
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ java/unlegacy/android/update/SystemUpdates.java
@@ -0,0 +1,20 @@
+package unlegacy.android.update;
+
+import android.app.Application;
+
+public class SystemUpdates extends Application {
+
+    private static boolean activityVisible;
+
+    public static boolean isActivityVisible() {
+        return activityVisible;
+    }
+
+    public static void activityResumed() {
+        activityVisible = true;
+    }
+
+    public static void activityPaused() {
+        activityVisible = false;
+    }
+}
