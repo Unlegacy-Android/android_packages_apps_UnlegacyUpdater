5b2d1a2b22e95f5bc33ce01e0cb213aac3836e1d
André Pinela <sheffzor@gmail.com>
2016-10-13 19:27:48 +0100

    Hail Unlegacy Android FOTA!!
    Change-Id: I8f1f77f5f0b900cbb1259381a41e3f4238c0831f
    Signed-off-by: André Pinela <sheffzor@gmail.com>

---
+++ Android.mk
@@ -0,0 +1,16 @@
+LOCAL_PATH:= $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_TAGS := optional
+# Reference java/ source files
+LOCAL_SRC_FILES := \
+    $(call all-java-files-under, java)
+# Re-map res/ directly
+LOCAL_RESOURCE_DIR := $(LOCAL_PATH)/app/src/main/res
+
+LOCAL_PACKAGE_NAME := UnlegacyUpdater
+LOCAL_SDK_VERSION := current
+LOCAL_PROGUARD_ENABLED := disabled
+LOCAL_CERTIFICATE := platform
+
+include $(BUILD_PACKAGE)
